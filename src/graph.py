5236aa1889e66c65118b763f60ded69e6800ab9a
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-06-20 16:48:40 -0400

    data for 2 vertexes and 1 edge created

--- src/graph.py
+++ src/graph.py
@@ -17,6 +17,11 @@ class Graph:
     
     def debug_create_test_data(self):
         debug_vertex_1 = Vertex('t1', x=40, y=40)
+        debug_vertex_2 = Vertex('t2', x=140, y=140)
         print("HI I AM FROM GRAPH.PY!", debug_vertex_1.pos['x'])
+        debug_edge_1 = Edge(debug_vertex_2)
+        debug_vertex_1.edges.append(debug_edge_1)
+
+        self.vertexes.extend([debug_vertex_1, debug_vertex_2])
 
 # # PICKUP FROM 20-30min left
