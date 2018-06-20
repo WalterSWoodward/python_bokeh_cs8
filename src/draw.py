a94b816663383bfcbd92b28ae26a11ff1c7b73ec
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-06-20 16:19:11 -0400

    Experimenting with Adding Vertex
    Adds 9th vertex, with new color appended to Spectral8 list.

--- src/draw.py
+++ src/draw.py
@@ -5,16 +5,28 @@ from bokeh.plotting import figure
 from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
 from bokeh.palettes import Spectral8
 
+# Cannot just increase this to get more vertexes
+# Bokeh works if you decrease the number though - it will render what it can in
+# the case of incomplete data.
 N = 9
 node_indices = list(range(N))
+
+# QUESTION: What is this??? - REVIEW
+# You cannot mutate Spectral8 directly, so you have to have two lines here.
+debug_pallete = Spectral8
+debug_pallete.append('#ff0000') # Just adds this color to the list of Spectral8 colors
+# debug_pallete.append('#ff0000')
+# debug_pallete += [a, b, c] # Can use this format as well to combine into one line
 
 plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,1.1), y_range=(-1.1,1.1),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
+# Adds node indices (N of them)
 graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(Spectral8, 'color')
+# Spectral8 is a list of colors
+graph.node_renderer.data_source.add(debug_pallete, 'color')
 graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
 
 graph.edge_renderer.data_source.data = dict(
@@ -23,7 +35,7 @@ graph.edge_renderer.data_source.data = dict(
 
 ### start of layout code
 # Looks like this is setting the positions of the vertexes
-circ = [i*2*math.pi/8 for i in node_indices]
+circ = [i*2*math.pi/N for i in node_indices]
 x = [math.cos(i) for i in circ]
 y = [math.sin(i) for i in circ]
 
---
+++ src/graph.py
@@ -0,0 +1,22 @@
+class Edge:
+    # In terms of OOP, this is "overriding" the built in init func
+    def __init__(self, destination):
+        self.destination = destination
+
+class Vertex:
+    # two stars means it will be a list of keyword arguments
+    def __init__(self, value, **pos): #TODO: Test default arguments
+        self.value = value
+        self.color = 'white'
+        self.pos = pos
+        self.edges = []
+
+class Graph:
+    def __init__(self):
+        self.vertexes = []
+    
+    # def debug_create_test_data(self):
+    #     debug_vertex_1 = Vertex('t1', x=40, y=40)
+    #     print(debug_vertex_1.pos['x'])
+
+# # PICKUP FROM 20-30min left
